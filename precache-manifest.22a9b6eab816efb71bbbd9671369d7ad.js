cd469ed536e760749001f68b79b76367411b2300
vivek <vivek.gopalan@myntra.com>
2018-12-29 20:02:54 +0530

    Updates

---
+++ precache-manifest.22a9b6eab816efb71bbbd9671369d7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "30d3b82113d5091c3871",
+    "url": "/testapp/static/js/runtime~main.30d3b821.js"
+  },
+  {
+    "revision": "a6f8cec1d50d8345b588",
+    "url": "/testapp/static/js/main.a6f8cec1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/testapp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a6f8cec1d50d8345b588",
+    "url": "/testapp/static/css/main.bbbd0b19.chunk.css"
+  },
+  {
+    "revision": "ba6328606962db376e8b7611aaae9c8e",
+    "url": "/testapp/index.html"
+  }
+];
